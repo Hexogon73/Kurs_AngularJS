411a4dbe18a20f25b7d61e9704dbba7df057f199
Hexogon73 <sergeylukianoff@yandex.ru>
2017-06-13 01:31:47 +0400

    Создан Грид, добавлена БД с тестовыми данными, реализована работа фильтров, поиск

---
+++ CarTable/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarTable.Models;
+
+namespace CarTable.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetCars()
+        {
+            using(CarDBEntities dc = new CarDBEntities())
+            {
+                var cars = dc.Cars.OrderBy(a => a.Brand).ToList();
+                return Json(new { data = cars }, JsonRequestBehavior.AllowGet);
+
+            }
+        }
+    }
+}
