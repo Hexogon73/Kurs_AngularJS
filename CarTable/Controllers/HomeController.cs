979210c531141f2ee4762da3783e495a6081dd0d
Hexogon73 <sergeylukianoff@yandex.ru>
2017-06-13 04:54:22 +0400

    добалены диалоговые окна

--- CarTable/Controllers/HomeController.cs
+++ CarTable/Controllers/HomeController.cs
@@ -24,5 +24,84 @@ namespace CarTable.Controllers
 
             }
         }
+
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            using (CarDBEntities dc = new CarDBEntities())
+            {
+                var v = dc.Cars.Where(a => a.CarID == id).FirstOrDefault();
+                return View(v);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Save(Car car)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using(CarDBEntities dc = new CarDBEntities())
+                {
+                    if (car.CarID > 0)
+                    {
+                        //edit
+                        var v = dc.Cars.Where(a => a.CarID == car.CarID).FirstOrDefault();
+                        if (v != null)
+                        {
+                            v.Brand = car.Brand;
+                            v.Model = car.Model;
+                            v.Number = car.Model;
+                            v.Registration_date = car.Registration_date;
+                            v.Owner = car.Owner;
+                            v.Registration_place = car.Registration_place;
+                        }
+                    }
+                    else
+                    {
+                        //save
+                        dc.Cars.Add(car);
+                    }
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            using (CarDBEntities dc = new CarDBEntities())
+            {
+                var v = dc.Cars.Where(a => a.CarID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    return View(v);
+                }
+                else
+                {
+                    return HttpNotFound();
+                }
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteCars(int id)
+        {
+            bool status = false;
+            using (CarDBEntities dc = new CarDBEntities())
+            {
+                var v = dc.Cars.Where(a => a.CarID == id).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Cars.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult {Data = new { status = status} };
+        }
     }
 }
---
+++ CarTable/Models/Extended/Cars.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarTable.Models
+{
+    public class Cars
+    {
+    }
+    public class CarsMetadata //проверка обязательных данных
+    {
+        [Required(AllowEmptyStrings =false, ErrorMessage = "Пожалуйста введите марку автомобиля")]
+        public string Brand { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Пожалуйста введите модель автомобиля")]
+        public string Model { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Пожалуйста введите номер автомобиля")]
+        public string Number { get; set; }
+    }
+}
